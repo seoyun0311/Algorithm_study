5508f54c12275d41aab58775b207062b3bedbef7
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-09-19 01:20:22 +0900

    week1_Thu

--- week1(Input/Ouput)/_11718_1.java
+++ week1(Input/Ouput)/_11718_1.java
@@ -4,8 +4,7 @@ public class _11718_1 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    // 입력이 있는 동안 계속해서 처리
-    while (sc.hasNextLine()) {
+    while (sc.hasNextLine()) { // 입력이 있는 동안 계속해서 처리
       String str = sc.nextLine(); // 입력을 한 줄씩 읽음
       System.out.println(str); 
     }
