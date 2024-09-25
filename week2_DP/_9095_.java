6a589d992c5a94c69cf717d277cb55ae3549aca9
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-09-25 22:06:01 +0900

    week2_Wed

---
+++ week2_DP/_9095_.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class _9095_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        
+        int[] dp = new int[12]; 
+        dp[0] = 1;  
+        dp[1] = 1; 
+        dp[2] = 2;  
+        dp[3] = 4;  
+        
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();  
+            System.out.println(dp[n]);  
+        }
+        
+        sc.close();
+    }
+}
