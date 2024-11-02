ec50c7912e4ef4612f8f5c0f7eeceb378e366d03
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-02 20:41:39 +0900

    week5_Sat

---
+++ week5_string/_2743_.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class _2743_ {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a.length());
+}
+}
