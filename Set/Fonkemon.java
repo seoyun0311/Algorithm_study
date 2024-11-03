fae1a7582723b00b7c4ec32269a4286f25397e7e
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-03 23:33:51 +0900

    Set

--- Set/Fonkemon.java
+++ Set/Fonkemon.java
@@ -1,22 +1,9 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+
 
 class Fonkemon {
-    public int solution(int[] nums) {
-        int answer = 0;
-        int count = nums.length / 2;
-        Set<Integer> set = new HashSet<>(); 
-        
-        for(int num : nums){
-            set.add(num); 
-        }
-
-        if(set.size() >= count){ 
-            answer = count; 
-        } else { 
-            answer = set.size(); 
-        }
-
-        return answer;
-    }
+  public int solution(int[] nums){
+    return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length / 2);
+  }
+   
 }
