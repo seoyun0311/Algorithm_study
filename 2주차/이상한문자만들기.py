b9c37d544ef0caaacfebbab09aa6e0ee095e0578
seoyun0311 <yseoy0311@gmail.com>
2025-05-20 19:38:56 +0900

    화_문자열

---
+++ 2주차/이상한문자만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    words = s.split(' ')  
+    result = []
+
+    for word in words:
+        new_word = ''
+        for i in range(len(word)):
+            if i % 2 == 0:
+                new_word += word[i].upper()
+            else:
+                new_word += word[i].lower()
+        result.append(new_word)
+
+    return ' '.join(result)
