b2007500549936c4e74ac3880b543dba48630afc
seoyun0311 <yseoy0311@gmail.com>
2025-09-09 00:24:49 +0900

    week1_월

--- BFS_DFS/DFS와BFS.py
+++ BFS_DFS/DFS와BFS.py
@@ -1,10 +1,9 @@
 import sys
-
-def dfs(idx) :
+def dfs(idx):
     global visited
     visited[idx] = True
-    print(idx, end = ' ')
-    for next in range(1, N+1) :
+    print(idx, end = '')
+    for next in range(1, N + 1):
         if not visited[next] and graph[idx][next]:
             dfs(next)
 
@@ -12,9 +11,8 @@ def bfs():
     global q, visited
     while q:
         cur = q.pop(0)
-        visited[cur] = True
         print(cur, end = ' ')
-        for next in range(1, N + 1) :
+        for next in range(1, N + 1):
             if not visited[next] and graph[cur][next]:
                 visited[next] = True
                 q.append(next)
@@ -23,20 +21,20 @@ def bfs():
 input = sys.stdin.readline
 N, M, V = map(int, input().split())
 
-graph = [[False] * (N + 1) for _ in range(N + 1)]
-visited = [False] * (N + 1)
+graph = [[False] * M for _ in range(N + 1)]
+visited = [False] * M
 
 # 1. graph 정보 입력
-for _ in range(M) :
+for _ in range(M):
     a, b = map(int, input().split())
     graph[a][b] = True
     graph[b][a] = True
 
-# 2. dfs 
+# 2. dfs
 dfs(V)
 print()
-
 # 3. bfs
-visited = [False] * (N + 1)
+visited = [False] * M
 q = [V]
+visited[V] = True
 bfs()
